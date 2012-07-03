1a890de3a1e02ed5cbb0f383a41ed9e000467430
Stephen Caudle <scaudle@doceme.com>
2012-07-03 15:23:01 -0400

    Code cleanup

--- gpiomodule.c
+++ gpiomodule.c
@@ -70,13 +70,11 @@ typedef struct {
 	int fd_val;	/* value fd */
 	int fd_dir;	/* direction fd */
 	int fd_edge;	/* interrupt edge fd  */
-	
+
 } GPIO;
 
 PyInterpreterState* interp;
 PyThread_type_lock lock;
-
-//typedef struct poll_cb_info;
 
 struct poll_cb_info {
 	PyObject * callback;
@@ -84,28 +82,11 @@ struct poll_cb_info {
 	int	idx;
 	struct poll_cb_info * next;
 };
+
 struct poll_cb_info * global_poll_cb_info;
 
 int global_nfds;
 struct pollfd global_pfd[16];
-
-// return 1 if deletion successful, 0 if not found
-
-
-#ifdef DEBUG_POLL
-static void dump_poll_cb() {
-	printf("dump_poll_cb() starting at %p\n", global_poll_cb_info);
-	struct poll_cb_info * poll_cb_info_iterator;
-	poll_cb_info_iterator = global_poll_cb_info;
-	int i = 0;
-
-	while (poll_cb_info_iterator != NULL) {
-		printf("dump_poll_cb(%i) fd = %i, callback = %p, next = %p\n", i, poll_cb_info_iterator->fd, poll_cb_info_iterator->callback, poll_cb_info_iterator->next );
-		poll_cb_info_iterator = poll_cb_info_iterator->next;
-		i++;
-	}
-}
-#endif
 
 static int del_poll_cb(int fd) {
 	struct poll_cb_info * poll_cb_info_iterator, *tmp;
@@ -125,8 +106,6 @@ static int del_poll_cb(int fd) {
 			} else {
 				tmp->next = poll_cb_info_iterator->next;
 				free(poll_cb_info_iterator);
-			//	poll_cb_info_iterator = NULL;
-			//	printf("del_poll_cb() done; global_nfds now: %i\n", global_nfds);
 				return 1;
 			}
 		} else {
@@ -134,38 +113,33 @@ static int del_poll_cb(int fd) {
 			poll_cb_info_iterator = poll_cb_info_iterator->next;
 		}
 	}
+
 	return 0;
 }
 
 static struct poll_cb_info * get_poll_cb(int fd) {
-//	printf("get_poll_cb() for fd %i\n", fd);
 	struct poll_cb_info * poll_cb_info_iterator;
 	poll_cb_info_iterator = global_poll_cb_info;
 
 	while (poll_cb_info_iterator != NULL) {
-//		printf("get_poll_cb() is poll_cb_info_iterator->fd (%i) == fd (%i) ?\n", poll_cb_info_iterator->fd, fd );
 		if (poll_cb_info_iterator->fd == fd) break;
 		poll_cb_info_iterator = poll_cb_info_iterator->next;
 	}
-/*	if(poll_cb_info_iterator != NULL)
-		printf("get_poll_cb() done; found\n");
-	else
-		printf("get_poll_cb() done; not found\n");
-*/	return poll_cb_info_iterator;
+	return poll_cb_info_iterator;
 }
 
 static int add_poll_cb(PyObject * callback, int fd)
 {
 	struct poll_cb_info * poll_cb_info_iterator;
 
-	if ( get_poll_cb(fd) != NULL ) return 0;
+	if ( get_poll_cb(fd) != NULL )
+		return 0;
 
 	poll_cb_info_iterator = malloc(sizeof(struct poll_cb_info));
-	if (poll_cb_info_iterator == NULL) return -1;
+	if (poll_cb_info_iterator == NULL)
+		return -1;
 
-//	printf("add_poll_cb(): setting next to %x\n", global_poll_cb_info);
 	poll_cb_info_iterator->next = global_poll_cb_info;
-//	printf("add_poll_cb(): setting global_poll_cb_info to %x\n", poll_cb_info_iterator);
 	global_poll_cb_info = poll_cb_info_iterator;
 
 	poll_cb_info_iterator->fd = fd;
@@ -175,7 +149,7 @@ static int add_poll_cb(PyObject * callback, int fd)
 	global_pfd[global_nfds].events = POLLPRI | POLLERR;
 	poll_cb_info_iterator->idx = global_nfds;
 	global_nfds++;
-//	printf("add_poll_cb() done; global_nfds now: %i\n", global_nfds);
+
 	return 0;
 }
 
@@ -196,12 +170,12 @@ static int getl( int fd, char * res )
 		res[i] = c;
 		i++;
 	}
+
 	return i;
 }
 
 static void t_bootstrap(void* rawself)
 {
-//	printf("Hello, iam Threadi :)\n");
 	PyObject * callback;
 	PyThreadState *tstate;
 	PyObject* args = NULL;
@@ -229,10 +203,8 @@ static void t_bootstrap(void* rawself)
  			PyErr_SetFromErrno(PyExc_IOError);
 			break;
 		}
-//		printf("Threadi: nfds: %i\n", global_nfds);
 		for (i = 0; i < global_nfds; i++) {
 			if (global_pfd[i].revents != 0) {
-//				fd_glow = global_pfd[i].fd;
 				lseek(global_pfd[i].fd, 0, SEEK_SET);
 				if (read(global_pfd[i].fd, &buf, 1 ) != 1) {
 					PyErr_SetFromErrno(PyExc_IOError);
@@ -241,11 +213,9 @@ static void t_bootstrap(void* rawself)
 				res = NULL;
 				args = Py_BuildValue("(i)", buf - 48); // we got ASCII '0'
 				callback = get_poll_cb(global_pfd[i].fd)->callback;
-//				printf("Threadi: fd: %i, revents: %i\n", global_pfd[i].fd, global_pfd[i].revents);
 				PyGILState_STATE s = PyGILState_Ensure();
 				res = PyObject_CallObject(callback, args);
 				PyGILState_Release(s);
-//				printf("Threadi :5\n");
 				Py_DECREF(args);
 				if (res == NULL) {
 					Py_XDECREF(res);
@@ -268,8 +238,6 @@ static void t_bootstrap(void* rawself)
 		}
 	}
 	PyThread_release_lock(lock);
-//	printf("terminating poll thread\n");
-//	PyEval_ReleaseThread(tstate);
 	lock = NULL;
 	PyThreadState_Clear(tstate);
 	PyThreadState_DeleteCurrent();
@@ -287,6 +255,7 @@ getDirection(GPIO * self) {
 	res = PyBytes_FromString(&buf[0]);
 
 	Py_INCREF(res);
+
 	return res;
 }
 
@@ -304,6 +273,7 @@ setDirection(GPIO * self, PyObject * cdir) {
 	}
 
 	Py_INCREF(Py_None);
+
 	return Py_None;
 }
 
@@ -318,6 +288,7 @@ getTrigger(GPIO * self) {
 	res = PyBytes_FromString(&buf[0]);
 
 	Py_INCREF(res);
+
 	return res;
 }
 
@@ -335,6 +306,7 @@ setTrigger(GPIO * self, PyObject * ctrig) {
 	}
 
 	Py_INCREF(Py_None);
+
 	return Py_None;
 }
 
@@ -355,6 +327,7 @@ int exportGpio( int gpio ) {
 		return -1;
 	}
 	close( fd );
+
 	return 0;
 }
 
@@ -365,8 +338,7 @@ GPIO_get_value(GPIO * self) {
 
 	getl(self->fd_val, &cval[0]);
 	i = PyInt_FromString(&cval[0], NULL, 10);
-	
-	// printf("\nget_value() returning %i (%i)\n", cval[0] - 48, PyInt_AsLong(i));
+
 	return i;
 
 }
@@ -386,7 +358,7 @@ GPIO_set_value(GPIO * self, PyObject * cval) {
 		PyErr_SetFromErrno( PyExc_IOError );
 		return -1;
 	}
-		
+
 	return 0;
 }
 
@@ -412,36 +384,22 @@ GPIO_new(PyTypeObject *type, PyObject *args, PyObject *kwds)
 static void
 GPIO_dealloc(GPIO *self)
 {
-//	printf("%i[%x]:GPIO_dealloc() pre - global_nfds: %i\n", self->gpio, self, global_nfds);
 	PyObject * cb = NULL;
 	struct poll_cb_info * poll_cb;
 
 	poll_cb = get_poll_cb(self->fd_val);
-//	printf("%i[%x]:GPIO_dealloc() 1\n", self->gpio, self);
 	if (poll_cb != NULL)
 		cb = poll_cb->callback;
-//	printf("%i[%x]:GPIO_dealloc() 2\n", self->gpio, self);
 	if ( cb != NULL ) {
-//		printf("pre-del:\n");
-#ifdef DEBUG_POLL
-		dump_poll_cb();
-#endif
 		del_poll_cb(self->fd_val);
-//		printf("post-del:\n");
-#ifdef DEBUG_POLL
-		dump_poll_cb();
-#endif
 		Py_DECREF( cb );
 	}
-//	printf("%i[%x]:GPIO_dealloc() 3\n", self->gpio, self);
 	Py_XDECREF(self->direction);
 	Py_XDECREF(self->trigger);
 	close(self->fd_val);
 	close(self->fd_dir);
 	close(self->fd_edge);
-//	printf("%i[%x]:GPIO_dealloc() post - global_nfds: %i\n", self->gpio, self, global_nfds);
 	PyObject_Del((PyObject*) self);
-//	self->ob_type->tp_free((PyObject *)self);
 }
 
 static int
@@ -449,7 +407,7 @@ GPIO_init(GPIO *self, PyObject *args, PyObject *kwds)
 {
 	int fd = -1;
 	int gpio = -1;
-	
+
 	PyObject * direction = NULL;
 	PyObject * trigger = NULL;
 	PyObject * tmp = NULL;
@@ -470,7 +428,6 @@ GPIO_init(GPIO *self, PyObject *args, PyObject *kwds)
 		// try to get gpio exported:
 		if ( exportGpio( gpio ) == 0 ) {
 			// check if export was (really) successful
-			
 			if ( ( fd = open( self->v_path, O_RDWR ) ) == -1) {
 				// export failed
 				PyErr_SetFromErrno( PyExc_IOError );
@@ -534,6 +491,7 @@ GPIO_get_direction( GPIO *self, void *closure )
 {
 	PyObject * result = ( PyObject * ) self->direction ;
 	Py_INCREF( result );
+
 	return result;
 }
 
@@ -565,6 +523,7 @@ GPIO_get_trigger( GPIO *self, void *closure )
 {
 	PyObject * result = ( PyObject * ) self->trigger;
 	Py_INCREF( result );
+
 	return result;
 }
 
@@ -597,6 +556,7 @@ GPIO_get_callback(GPIO *self, void *closure)
 	if (result == NULL)
 		result = Py_None;
 	Py_INCREF(result);
+
 	return result;
 }
 
@@ -612,43 +572,30 @@ GPIO_set_callback(GPIO *self, PyObject *val, void *closure)
 	if (poll_cb != NULL)
 		cb = poll_cb->callback;
 
-	if ( cb == val ) return 0;
+	if ( cb == val )
+		return 0;
 	if ( cb != NULL ) {
-//		printf("pre-del:\n");
-#ifdef DEBUG_POLL
-		dump_poll_cb();
-#endif
 		del_poll_cb(self->fd_val);
-//		printf("post-del:\n");
-#ifdef DEBUG_POLL
-		dump_poll_cb();
-#endif
 		Py_DECREF( cb );
 	}
-	
-	if ( val == Py_None ) return 0;
-	
+
+	if ( val == Py_None )
+		return 0;
+
 	if ( !PyCallable_Check( val ) != 0 ) {
 		PyErr_SetString(PyExc_TypeError, "set_callback: parameter must be a callable python object (function, method, class) or None");
 		return -1;
 	}
 
 	Py_INCREF(val);
-//	printf("pre-add:\n");
-#ifdef DEBUG_POLL
-	dump_poll_cb();
-#endif
+
 	add_poll_cb(val, self->fd_val);
-//	printf("post-add:\n");
-#ifdef DEBUG_POLL
-	dump_poll_cb();
-#endif
-	
+
 	if (global_nfds == 1 && lock == NULL) {
 		PyEval_InitThreads(); /* Start the interpreter's thread-awareness */
 		interp = PyThreadState_Get()->interp;
 		lock = PyThread_allocate_lock();
-		if (lock == NULL) 
+		if (lock == NULL)
 			return -1;
 		ident = PyThread_start_new_thread(t_bootstrap, (void*) self);
 		if (ident == -1) {
@@ -659,11 +606,6 @@ GPIO_set_callback(GPIO *self, PyObject *val, void *closure)
 
 	return 0;
 }
-
-//+	{ "none",    0 },
-//+	{ "falling", BIT(FLAG_TRIG_FALL) },
-//+	{ "rising",  BIT(FLAG_TRIG_RISE) },
-//+	{ "both",    BIT(FLAG_TRIG_FALL) | BIT(FLAG_TRIG_RISE) },
 
 static PyGetSetDef GPIO_getset[] = {
 	{"value", (getter)GPIO_get_value, (setter)GPIO_set_value,
